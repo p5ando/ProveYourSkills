f622f71dac3ed8a7b59e72c11e26e0d29c98da01
p5ando <sandopredrag95@gmail.com>
2025-01-29 02:17:58 +0100

    initialize DI

---
+++ ProveYourSkills/ProveYourSkills/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProveYourSkills
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IServiceProvider? _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // setup di container
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // open window
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        // register service
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Configure Logging
+            services.AddLogging();
+
+            // Register Views
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+            // Dispose of services if needed
+            if (_serviceProvider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
---
+++ ProveYourSkills/ProveYourSkills/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ProveYourSkills
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(IMainViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+
+    public interface IMainViewModel
+    {
+        ICommand SaveCommand { get; }
+    }
+
+    public class MainViewModel : IMainViewModel
+    {
+        private readonly ILogger<MainViewModel> _logger;
+
+        public MainViewModel(ILogger<MainViewModel> logger)
+        {
+            _logger = logger;
+        }
+
+        private ICommand _saveCommand;
+        public ICommand SaveCommand =>
+            _saveCommand ??= new AsyncRelayCommand(ExecuteSaveAsync);
+
+        private async Task ExecuteSaveAsync()
+        {
+            try
+            {
+                _logger.LogInformation("Save command executed");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error during save operation");
+            }
+        }
+    }
+}
